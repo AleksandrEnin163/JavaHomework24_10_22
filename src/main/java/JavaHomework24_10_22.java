b87f677d73a47f2486dd57561991f150c67cc62f
alexenin163 <alexenin163@gmail.com>
2022-10-25 10:27:23 +0200

    first commit

---
+++ src/main/java/JavaHomework24_10_22.java
@@ -0,0 +1,9 @@
+public class JavaHomework24_10_22 {
+    public static void main(String args[]) {
+        int count = 0;
+        for(int i = 0; i < 163; i = i + 5 - 1) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
